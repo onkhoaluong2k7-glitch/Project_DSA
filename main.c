0d6f55c4ef8c384ecb982ce4701f0d0add381743
OKLng <onkhoaluong2k7@gmail.com>
2026-04-14 07:55:32 +0700

    fix bug

--- main.c
+++ main.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
-#include "sqlite3.h"
+
 //====================================================================
 //                  KHAI BAO CAC BIEN, CAC KIEU DU LIEU
 //====================================================================
